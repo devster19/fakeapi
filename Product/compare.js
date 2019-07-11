c58c451d4f334de5efba54b1c9fbc14424bb8903
Devster Hiddenman <48503935+devster19@users.noreply.github.com>
2019-07-11 13:41:31 +0700

    add compare file

---
+++ Product/compare.js
@@ -0,0 +1,56 @@
+const fakeApi =[
+    {
+      name:'AAA',
+      point:4.5,
+      reviewer:40,
+      price:800,
+      source:'USA',
+      type:['AAAAA','Olive Extract','Nature X','Nature Y'],
+      ingredient:[
+        {name:'xsdfs',available:true},
+        {name:'rrrejjjjjfse',available:true},
+        {name:'xss',available:false},
+        {name:'xqqq',available:true},
+        {name:'xvsde',available:false},
+        {name:'xsffdfw',available:false},
+        {name:'xewe',available:false},
+        {name:'xggefw',available:true}
+      ]
+    },
+    {
+      name:'BBB',
+      point:4.5,
+      reviewer:49,
+      price:599,
+      source:'USA',
+      type:['BBBBB','Olive Extract','Nature X','Nature Y'],
+      ingredient:[
+        {name:'xsdfs',available:false},
+        {name:'rrrefse',available:true},
+        {name:'xss',available:false},
+        {name:'xqqq',available:true},
+        {name:'xvsde',available:true},
+        {name:'xsffdfw',available:false},
+        {name:'xewe',available:false},
+        {name:'xggefw',available:true}
+      ]
+    },
+    {
+      name:'CCC',
+      point:4,
+      reviewer:34,
+      price:1200,
+      source:'USA',
+      type:['CCCCCC','Olive Extract','Nature X','Nature Y'],
+      ingredient:[
+        {name:'xsdfs',available:false},
+        {name:'rrrefse',available:true},
+        {name:'xss',available:false},
+        {name:'xqqq',available:true},
+        {name:'xvsde',available:true},
+        {name:'xsffdfw',available:false},
+        {name:'xewe',available:true},
+        {name:'xggefw',available:true}
+      ]
+    }
+  ]
